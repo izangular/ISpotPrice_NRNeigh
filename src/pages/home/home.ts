18776bb3d6c5acc200fb6c7713e72ebbaca2489a
Pauskar <pauskar@iazi.ch>
2017-08-08 16:24:27 +0530

    opening google map

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+declare var google;
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,30 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+   ionViewDidLoad(){
+     console.log('test');
+    this.loadMap();
+  }
+ 
+  loadMap(){
+ 
+    let latLng = new google.maps.LatLng(47.40799130707436, 8.555858796993675);
+ 
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+ 
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+ 
   }
 
 }

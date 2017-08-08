489f6517a476d6846cb9d2087ef0a99ff15a3741
Pauskar <pauskar@iazi.ch>
2017-08-08 16:43:14 +0530

    autocomplete

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, NgZone } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 declare var google;
@@ -12,27 +12,58 @@ export class HomePage {
   @ViewChild('map') mapElement: ElementRef;
   map: any;
 
-  constructor(public navCtrl: NavController) {
+  autocompleteItems;
+  autocomplete;
+  service = new google.maps.places.AutocompleteService();
 
+  constructor(public navCtrl: NavController, private zone: NgZone) {
+    this.autocompleteItems = [];
+    this.autocomplete = {
+      query: ''
+    };
   }
 
-   ionViewDidLoad(){
-     console.log('test');
+dismiss() {
+    //this.viewCtrl.dismiss();
+  }
+
+  chooseItem(item: any) {
+    //this.viewCtrl.dismiss(item);
+  }
+
+  updateSearch() {
+    if (this.autocomplete.query == '') {
+      this.autocompleteItems = [];
+      return;
+    }
+    let me = this;
+    this.service.getPlacePredictions({ input: this.autocomplete.query,  componentRestrictions: {country: 'CH'} }, function (predictions, status) {
+      me.autocompleteItems = []; 
+      me.zone.run(function () {
+        predictions.forEach(function (prediction) {
+          me.autocompleteItems.push(prediction.description);
+        });
+      });
+    });
+  }
+
+  ionViewDidLoad() {
+    console.log('test');
     this.loadMap();
   }
- 
-  loadMap(){
- 
+
+  loadMap() {
+
     let latLng = new google.maps.LatLng(47.40799130707436, 8.555858796993675);
- 
+
     let mapOptions = {
       center: latLng,
       zoom: 15,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     }
- 
+
     this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
- 
+
   }
 
 }

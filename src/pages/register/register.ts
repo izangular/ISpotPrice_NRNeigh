a5880bc4f46ca16ab5e6e55719f05a543bab173c
Pauskar <pauskar@iazi.ch>
2017-08-21 14:11:15 +0530

    code changes

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,5 +1,5 @@
 import { OnInit, Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController, } from 'ionic-angular';
 import { HomePage } from '../../pages/home/home';
 import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { ApiService } from '../../providers/api-service';
@@ -8,7 +8,7 @@ import { UniqueDeviceID } from '@ionic-native/unique-device-id';
 @Component({
     selector: 'register',
     templateUrl: 'register.html',
-    providers: [ApiService,UniqueDeviceID]
+    providers: [ApiService, UniqueDeviceID]
 })
 
 export class Register {
@@ -16,10 +16,10 @@ export class Register {
     registerform: FormGroup;
     submitAttempt: boolean = false;
     errorMessage: string;
-    deviceId:string;
+    deviceId: string;
 
     constructor(private navCtrl: NavController, private formBuilder: FormBuilder, private apiService: ApiService,
-        private uniqueDeviceID: UniqueDeviceID) {
+        private uniqueDeviceID: UniqueDeviceID, private loadingCtrl: LoadingController) {
         this.registerform = formBuilder.group({
             firstName: ['', Validators.required],
             lastName: ['', Validators.required],
@@ -31,40 +31,62 @@ export class Register {
     }
 
     register() {
-        this.submitAttempt = true;
+
         //  this.uniqueDeviceID.get()
         //                     .then((uuid: any) => this.deviceId = uuid )
         //                     .catch((error: any) => console.log(error));
 
         // console.log("deviceID: " + this.deviceId);
 
-        if (!this.registerform.valid) {
-            console.log("Invalid");
-        }
-        else {
-            this.apiService.register(this.registerform.controls.firstName.value, this.registerform.controls.lastName.value, this.registerform.controls.email.value,
-                this.registerform.controls.phoneNumber.value, this.deviceId).subscribe(
-                (data) => {
-                    if (data == "Success") {
-                        console.log("success!");
-                        localStorage.setItem("register","true");
-                        console.log("RegisterTest: " + localStorage.getItem("register"));
-                        this.navCtrl.push(HomePage);                      
+
+        try {
+            this.submitAttempt = true;
+            if (!this.registerform.valid) {
+                console.log("Invalid");
+                //loading.dismiss();
+            }
+            else {
+
+                let loading = this.loadingCtrl.create({
+                    spinner: 'bubbles',
+                    content: 'Please wait...'
+                });
+
+                loading.present();
+                this.apiService.register(this.registerform.controls.firstName.value, this.registerform.controls.lastName.value, this.registerform.controls.email.value,
+                    this.registerform.controls.phoneNumber.value, this.deviceId).subscribe(
+                    (data) => {
+                        if (data == "Success") {
+                            console.log("success!");
+                            localStorage.setItem("register", "true");
+                            console.log("RegisterTest: " + localStorage.getItem("register"));
+                            setTimeout(() => {
+                                loading.dismiss();
+                                this.navCtrl.push(HomePage);
+                            }, 3000)
+
+                        }
+                        else {
+                            loading.dismiss();
+                        }
+                    },
+                    (error) => {
+                        console.log(error);
+                        loading.dismiss(); 
+                        this.errorMessage = "Registration Failed";
                     }
-                },
-                (error) => {
-                    console.log(error);
-                    this.errorMessage = "Registration Failed";
-                }
-                )
-            console.log(this.registerform.controls.firstName.value);
-            console.log("success!");
-            //this.navCtrl.push(HomePage);
-
+                    )
+                console.log(this.registerform.controls.firstName.value);
+                console.log("success!");
+                //this.navCtrl.push(HomePage);
+            }
         }
-
-
-
+        catch (e) {
+            //loading.dismiss();
+        }
+        finally {
+            //loading.dismiss();
+        }
 
     }
 }
